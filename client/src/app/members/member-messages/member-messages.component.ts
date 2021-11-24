2cd632bdfcdb6cc8c09768da5083cc4551d44076
Joel4matrix <joel@4matrix.com>
2021-11-24 16:12:09 +0000

    End of part 193

---
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -0,0 +1,22 @@
+
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { MessageService } from 'src/app/_services/message.service';
+
+@Component({
+  selector: 'app-member-messages',
+  templateUrl: './member-messages.component.html',
+  styleUrls: ['./member-messages.component.css']
+})
+export class MemberMessagesComponent implements OnInit {
+  @Input() messages: Message[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  
+}
